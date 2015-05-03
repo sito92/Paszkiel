8b93568b52a5ca5a2f9f2c95881384d6aed40d38
Artur Stelmach <arturstelmach92@gmail.com>
2015-05-03 22:15:34 +0200

    Lay

--- SystemEkspercki/SystemEkspercki/Controllers/HomeController.cs
+++ SystemEkspercki/SystemEkspercki/Controllers/HomeController.cs
@@ -12,9 +12,9 @@ namespace SystemEkspercki.Controllers
     public class HomeController : BaseController
     {
         private const string UserPreferences = "viewModel";
-        public ActionResult Index(int id =1)
+        public ActionResult Index(int id = 1)
         {
-            UserPreferencesViewModel model=GetDefaultModel();
+            UserPreferencesViewModel model = GetDefaultModel();
             if (Session[UserPreferences] == null)
             {
 
@@ -41,53 +41,65 @@ namespace SystemEkspercki.Controllers
             {
                 model.Answers.Add(viewModel.QuestionId, viewModel.Answer);
             }
-            if (model.QuestionId>model.MaxQuestionId)
+            if (model.QuestionId > model.MaxQuestionId)
             {
                 return RedirectToAction("Summary");
             }
             else
             {
-                return RedirectToAction("Index",new {id = model.QuestionId});
+                return RedirectToAction("Index", new { id = model.QuestionId });
             }
-           
+
         }
 
         public ActionResult Summary()
         {
             var model = (UserPreferencesViewModel)Session[UserPreferences];
-            Dictionary<Laptop,int> laptopsWithSummary = new Dictionary<Laptop, int>();
+            Dictionary<Laptop, int> laptopsWithSummary = new Dictionary<Laptop, int>();
             foreach (var laptop in context.Laptops)
             {
                 laptopsWithSummary.Add(laptop, 0);
             }
+
             foreach (var answer in model.Answers)
             {
                 foreach (var laptop in context.Laptops)
                 {
-                    var question = context.Questions.FirstOrDefault(x => x.Id == answer.Key);
-                   
+                    var question = context.Questions
+                        .FirstOrDefault(x => x.Id == answer.Key);
+
                     if (question.GetType() == typeof(BoolQuestion))
                     {
-                        var laptopAnswerValue = context.LaptopBoolValues.Where(x => x.BoolQuestionId == answer.Key).Where(x=>x.LaptopId==laptop.Id).Select(x => x.Value).FirstOrDefault();
+                        var laptopAnswerValue = context.LaptopBoolValues
+                            .Where(x => x.BoolQuestionId == answer.Key)
+                            .Where(x => x.LaptopId == laptop.Id)
+                            .Select(x => x.Value)
+                            .FirstOrDefault();
+
                         if (Convert.ToInt32(laptopAnswerValue) != answer.Value)
                         {
                             if (laptopsWithSummary.ContainsKey(laptop))
-                            laptopsWithSummary.Remove(laptop);
+                                laptopsWithSummary.Remove(laptop);
                         }
-                        
+
                     }
                     else
                     {
-                        var laptopAnswerValue = context.LaptopFuzzyValues.Where(x => x.FuzzyQuestionId == answer.Key).Where(x=>x.LaptopId==laptop.Id).Select(x => x.Value).FirstOrDefault();
+                        var laptopAnswerValue = context.LaptopFuzzyValues
+                            .Where(x => x.FuzzyQuestionId == answer.Key)
+                            .Where(x => x.LaptopId == laptop.Id)
+                            .Select(x => x.Value)
+                            .FirstOrDefault();
+
                         if (laptopsWithSummary.ContainsKey(laptop))
-                        laptopsWithSummary[laptop] += answer.Value*laptopAnswerValue;
+                            laptopsWithSummary[laptop] += answer.Value * laptopAnswerValue;
                     }
-                    
-                }   
+
+                }
             }
 
             Session[UserPreferences] = null;
-            return View(laptopsWithSummary.OrderByDescending(x => x.Value).Select(x=>x.Key));
+            return View(laptopsWithSummary.OrderByDescending(x => x.Value).Select(x => x.Key));
         }
         public ActionResult About()
         {
@@ -103,13 +115,13 @@ namespace SystemEkspercki.Controllers
             return View();
         }
 
-        private UserPreferencesViewModel GetDefaultModel(int id =1)
+        private UserPreferencesViewModel GetDefaultModel(int id = 1)
         {
-           var model = new UserPreferencesViewModel()
-            {
-                QuestionId = id,
-                QuestionsIds = context.Questions.Select(x => x.Id).ToList(),
-            };
+            var model = new UserPreferencesViewModel()
+             {
+                 QuestionId = id,
+                 QuestionsIds = context.Questions.Select(x => x.Id).ToList(),
+             };
             return model;
         }
     }
